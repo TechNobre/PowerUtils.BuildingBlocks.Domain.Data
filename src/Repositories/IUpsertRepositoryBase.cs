7ecddf09d07f58c026e2c44eb81284f483784980
Nelson Nobre <technobre@gmail.com>
2022-08-21 11:28:28 +0100

    feat: Add methods `Upsert` and `UpsertAsync`

---
+++ src/Repositories/IUpsertRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using PowerUtils.BuildingBlocks.Domain;
+
+namespace PowerUtils.BuildingBlocks.Data.Repositories
+{
+    public interface IUpsertRepositoryBase<TEntity, TId> :
+        IEntityRepositoryBase<TEntity, TId>
+
+        where TEntity : IAggregateRoot<TId>
+    {
+        TId Upsert(TEntity entity);
+    }
+}
