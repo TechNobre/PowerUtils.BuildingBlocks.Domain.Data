b18288e4c0ff78c405cf2976e1d5bdf018e88828
Nelson Nobre <technobre@gmail.com>
2024-03-03 02:08:47 +0000

    fix: Marked all implementations as `Obsolete`
    BREAKING CHANGE: Deprecated package

--- src/AsyncRepositories/IEntityAsyncRepositoryBase.cs
+++ src/AsyncRepositories/IEntityAsyncRepositoryBase.cs
@@ -1,8 +1,10 @@
-﻿using PowerUtils.BuildingBlocks.Data.Repositories;
+﻿using System;
+using PowerUtils.BuildingBlocks.Data.Repositories;
 using PowerUtils.BuildingBlocks.Domain;
 
 namespace PowerUtils.BuildingBlocks.Data.AsyncRepositories
 {
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public interface IEntityAsyncRepositoryBase<TEntity, TId> :
         IRepositoryBase
 
--- src/Repositories/ICountRepositoryBase.cs
+++ src/Repositories/ICountRepositoryBase.cs
@@ -1,7 +1,9 @@
-﻿using PowerUtils.BuildingBlocks.Domain;
+﻿using System;
+using PowerUtils.BuildingBlocks.Domain;
 
 namespace PowerUtils.BuildingBlocks.Data.Repositories
 {
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public interface ICountRepositoryBase<TEntity, TId> :
         IEntityRepositoryBase<TEntity, TId>
 
